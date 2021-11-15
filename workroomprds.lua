adaa978f0dba1e347fc5c0c7ae0ccb12dd334aff
jss <j19sch@gmail.com>
2021-11-15 17:21:11 +0100

    moves puzzle22 ui elements to separate file

--- workroomprds.lua
+++ workroomprds.lua
@@ -1,3 +1,5 @@
+require 'ui-elements'
+
 function setup_puzzle_22()
     button_1 = define_button(false, 100, 110, 25)
     light_1 = define_light(false, 250, 110, 25)
@@ -20,59 +22,5 @@ function keypressed_puzzle_22(key)
     if key == "up" then
         toggle_light(light_1)
         toggle_button(button_1)
-    end
-end
-
-function define_button(state, x, y, radius)
-    local button = {}
-    button['state'] = state
-    button['x'] = x
-    button['y'] = y
-    button['radius'] = radius
-
-    return button
-end
-
-function draw_button(button)
-    if button['state'] == true then
-        love.graphics.circle("fill", button['x'], button['y'], button['radius'])
-    else
-        love.graphics.circle("line", button['x'], button['y'], button['radius'])
-    end
-end
-
-function toggle_button(button)
-    if button['state'] == true then
-        button['state'] = false
-    else
-        button['state'] = true
-    end
-end
-
-
-function define_light(state, x, y, radius)
-    local light = {}
-    light['state'] = state
-    light['x'] = x
-    light['y'] = y
-    light['radius'] = radius
-
-    return light
-end
-
-function draw_light(light)
-    if light['state'] == true then
-        love.graphics.circle("fill", light['x'], light['y'], light['radius'])
-    else
-        love.graphics.circle("line", light['x'], light['y'], light['radius'])
-    end
-end
-
-
-function toggle_light(light)
-    if light['state'] == true then
-        light['state'] = false
-    else
-        light['state'] = true
     end
 end
